19931212f0672d9aebf0965a3eb3557eb0b19c53
Austin <freeluck.v.i.p999@gmail.com>
2025-10-28 17:06:28 -0500

    ran 'cargo run'

---
+++ idkoc/target/debug/deps/idkoc-03d5981c6b274827.d
@@ -0,0 +1,5 @@
+/home/austin/github-projects/idkoc/idkoc/target/debug/deps/idkoc-03d5981c6b274827.d: src/main.rs
+
+/home/austin/github-projects/idkoc/idkoc/target/debug/deps/idkoc-03d5981c6b274827: src/main.rs
+
+src/main.rs:
---
+++ idkoc/target/debug/idkoc.d
@@ -0,0 +1,1 @@
+/home/austin/github-projects/idkoc/idkoc/target/debug/idkoc: /home/austin/github-projects/idkoc/idkoc/src/main.rs
